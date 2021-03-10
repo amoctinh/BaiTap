cbf4542f9fb15cb78b16cc042576ec2c708fe43c
amoctinh <64021841+amoctinh@users.noreply.github.com>
2021-03-10 21:50:37 +0700

    bt

---
+++ bt3.py
@@ -0,0 +1,19 @@
+
+print('Nhập thông tin :')
+hoten=input('họ tên: ')
+ns=input('năm sinh: ')
+quequan=input('quê quán: ')
+noio=input('nơi ở: ')
+truong=input('trường: ')
+chuyennganh=input('chuyên ngành: ')
+
+sv={'hoten':hoten,'ns':ns,'quequan':quequan,'noio':noio,'truong':truong,'chuyennganh':chuyennganh}  
+print('thông tin vừa nhập  : ')
+print(sv['hoten'],sv['ns'],sv['quequan'],sv['noio'],sv['truong'],sv['chuyennganh'])
+
+
+
+
+
+
+
